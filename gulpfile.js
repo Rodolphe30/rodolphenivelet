9b12999b34cfd902b3bf464164614d394cd6f35f
Rodolphe NIVELET <rodolphe.nivelet@mail-metrics.com>
2016-09-14 19:09:11 +0200

    repaired

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,13 @@ gulp.task('default', function() {
 
 gulp.task('styles', function() {
 	var postcss      = require('gulp-postcss');
-    var sourcemaps   = require('gulp-sourcemaps');
-    var autoprefixer = require('autoprefixer-core');
+  var sourcemaps   = require('gulp-sourcemaps');
+  var autoprefixer = require('autoprefixer');
 
-    return gulp.src('sass/style.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.init())
-        .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./css/'));
+  return gulp.src('sass/style.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(sourcemaps.init())
+      .pipe(postcss([ require('autoprefixer') ]))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('./css/'));
 });
